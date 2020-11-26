8542dabb5c9fcf35121ff66bfaf8012f30b1d44b
Mariem BEN_CHARRADA <mariem.ben_charrada@front-in1.cemef>
2020-11-26 12:52:20 +0100

    implemented kmeans parallelisation with MPI

---
+++ TPs/ParallelProgrammingTP/src/IMGProcessing/Kmean_mpi.h
@@ -0,0 +1,229 @@
+
+// INIT CENTROID
+std::vector<uchar> init_centroid(std::vector<uchar> flat_image, int nb_channels)
+{
+	std::vector<uchar> cent(nb_channels);
+	int random_index;
+	switch(nb_channels)
+	{
+	  case(1):
+	  {
+	     random_index = rand() % flat_image.size();
+	     cent[0] = flat_image[random_index]; 
+	     break;
+	  }
+	  case(3):
+	  {
+	    // we pick a random index multiple of 3, to have all the colors of the pixel
+	    random_index = (rand() % (flat_image.size() / 3 )) * 3;
+	    cent[0] = flat_image[random_index];
+	    cent[1] = flat_image[random_index+1]; 
+	    cent[2] = flat_image[random_index+2]; 
+	  }
+	}
+	return cent;
+}
+
+
+// NEAREST CENTROID
+int nearest_centroid(int nb_channels, int nb_centroids, std::vector<uchar> centroids, uchar red, uchar green = 0, uchar blue = 0)
+{
+	std::vector<double> distances(nb_centroids);
+	double dist = 0;
+	for(int i=0; i<nb_centroids; i++)
+	{
+	  switch(nb_channels)
+	  {
+	    case(1):
+	    {
+		dist = abs(red - centroids[i]);
+		distances.at(i) = dist;
+		break;
+            }
+	    case(3):
+	    {
+		double diff_red = pow(red - centroids[3*i], 2);
+		double diff_green = pow(green - centroids[3*i+1], 2);
+		double diff_blue = pow(blue - centroids[3*i+2], 2);
+		dist = sqrt(diff_red + diff_green + diff_blue);
+		distances.at(i) = dist;
+		break;
+	    }
+	  }
+	}
+
+	// index of the minimum distance
+	double min = *std::min_element(distances.begin(), distances.end());
+	std::vector<double>::iterator itr = std::find(distances.begin(), distances.end(), min);
+	return std::distance(distances.begin(), itr);
+}
+
+
+// COUNT PIXELS FOR EACH CENTROIDS
+std::vector<double> count_pixels(int nb_centroids, std::vector<int> pixel_collection, std::vector<uchar> flat_image, int nb_rows, int nb_cols, int nb_channels)
+{
+	std::vector<double> count_res((nb_channels+1)*nb_centroids);
+	for(int j=0; j<nb_centroids; j++)
+	{
+	  double sum=0, sum_red=0, sum_green=0, sum_blue=0;
+	  int count=0;
+	  for(int i=0; i<nb_cols*nb_rows; i++)
+	  {
+	    if(pixel_collection[i]==j)
+	    {
+	      switch(nb_channels)
+	      {
+		case(1):
+		{
+			sum += (double) flat_image[i];
+			count ++;
+			break;
+		}
+		case(3):
+		{
+			sum_red += (double) flat_image[3*i];
+			sum_green += (double) flat_image[3*i+1];
+			sum_blue += (double) flat_image[3*i+2];
+			count ++;
+			break;
+		}
+	      }
+	    }
+	  }
+	  switch(nb_channels)
+	  {
+	    case(1):
+	    {
+		count_res[2*j] = sum;
+		count_res[2*j+1] = count;
+		break;
+	    }
+	    case(3):
+	    {
+		count_res[4*j] = sum_red;
+		count_res[4*j+1] = sum_green;
+		count_res[4*j+2] = sum_blue;
+		count_res[4*j+3] = count;
+		break;
+	    }
+	  }
+	}
+
+	return count_res;
+}
+
+// CALCULALTE BARYCENTER
+std::vector<uchar> compute_barycenter_and_update_centroids(std::vector<double> count_pxl, int nb_centroids, int nb_channels)
+{
+	std::vector<uchar> centroids(nb_centroids*nb_channels);
+	for(int i=0; i<nb_centroids; i++)
+	{
+	  switch(nb_channels)
+	  {
+	    case(1):
+	    {
+		double barycenter = (double) count_pxl[2*i] / (double) count_pxl[2*i+1];
+		centroids[i] = (uchar) barycenter;
+		break;
+	    }
+	    case(3):
+	    {
+		double barycenter_red = (double) count_pxl[4*i] / (double) count_pxl[4*i+3];
+		centroids[3*i] = (uchar) barycenter_red;
+		double barycenter_green = (double) count_pxl[4*i+1] / (double) count_pxl[4*i+3];
+		centroids[3*i+1] = (uchar) barycenter_green;
+
+		double barycenter_blue = (double) count_pxl[4*i+2] / (double) count_pxl[4*i+3];
+		centroids[3*i+2] = (uchar) barycenter_blue;
+
+
+	    }
+	  }
+	}
+
+	return centroids;
+}
+
+// COMPUTE VARIANCE
+double compute_variance(std::vector<uchar> old_centroids, std::vector<uchar> centroids, int nb_channels, int nb_centroids)
+{
+	double sum=0;
+	for(int i=0; i<nb_centroids; i++)
+	{
+	  switch(nb_channels)
+	  {
+	    case(1):
+	    {
+		sum += abs(old_centroids[i] - centroids[i]);
+		break;
+	    }
+	    case(3):
+	    {
+		double d_red = pow(old_centroids[3*i] - centroids[3*i], 2);
+		double d_green = pow(old_centroids[3*i+1] - centroids[3*i+1], 2);
+		double d_blue = pow(old_centroids[3*i+2] - centroids[3*i+2], 2);
+		sum += sqrt(d_red + d_green + d_blue);
+		break;
+	    }
+	  }
+	}
+	return (double) sum / (double) nb_centroids;
+}
+
+// COMPUTE SEGMENTATION
+
+void compute_segmentation(std::vector<uchar>& flat_image, int nb_rows, int nb_cols, int nb_channels, int nb_centroids, std::vector<uchar> centroids)
+{
+	for(int i=0; i<nb_cols*nb_rows; i++)
+	{
+	  switch(nb_channels)
+	  {
+	    case(1):
+	    {
+	      uchar pixel = flat_image[i];
+	      int id_centroid = nearest_centroid(nb_channels, nb_centroids, centroids, pixel);
+	      flat_image[i] = centroids[id_centroid];
+	      break;
+	    }
+	    case(3):
+	    {
+	      uchar red = flat_image[3*i];
+	      uchar green = flat_image[3*i+1];
+	      uchar blue = flat_image[3*i+2];
+	      int id_centroid = nearest_centroid(nb_channels, nb_centroids, centroids, red, green, blue);
+	      flat_image[3*i] = centroids[3*id_centroid];
+	      flat_image[3*i+1] = centroids[3*id_centroid+1];
+	      flat_image[3*i+2] = centroids[3*id_centroid+2];
+	      break;
+	    }
+	  }
+	}
+}
+
+// UNFLAT IMAGE
+void unflat_image(std::vector<uchar> flat_image, cv::Mat& image, int nb_channels)
+{
+	using namespace cv;
+	for(int i=0; i<image.rows; i++)
+	{
+	  for(int j=0; j<image.cols; j++)
+	  {
+	    switch(nb_channels)
+	    {
+	      case(1):
+	      {
+		image.at<uchar>(i,j) = flat_image[i*image.cols+j];
+		break;
+	      }
+	      case(3):
+	      {
+		Mat_<Vec3b> _I = image;
+		_I(i,j)[0] = 3*(i*image.cols+j);
+		_I(i,j)[1] = 3*(i*image.cols+j)+1;
+		_I(i,j)[2] = 3*(i*image.cols+j)+2;
+		break;
+	      }
+	    }
+	  }
+	}
+}
--- TPs/ParallelProgrammingTP/src/TP4/img_kmean_mpi.cpp
+++ TPs/ParallelProgrammingTP/src/TP4/img_kmean_mpi.cpp
@@ -18,9 +18,12 @@
 #include <boost/program_options/parsers.hpp>
 #include <boost/program_options/cmdline.hpp>
 #include <boost/program_options/variables_map.hpp>
+#include <IMGProcessing/Kmean_mpi.h>
 
 using namespace cv;
 using namespace std;
+
+
 
 int main( int argc, char** argv )
 {
@@ -30,10 +33,8 @@ int main( int argc, char** argv )
         ("help", "produce help")
         ("file",value<std::string>(), "image file")
         ("show",value<int>()->default_value(0), "show image")
-        ("add-noise",value<int>()->default_value(0), "add noise")
-        ("noise-density",value<int>()->default_value(0), "noise density")
-        ("filter",value<int>()->default_value(0), "filtre median")
-        ("to-gray",value<int>()->default_value(0), "create gray image") ;
+        ("seg",value<int>()->default_value(1), "kmeans segmentation")
+        ("kmean-value",value<int>(), "Kmeans K value")	;
     variables_map vm;
     store(parse_command_line(argc, argv, desc), vm);
     notify(vm);
@@ -49,7 +50,7 @@ int main( int argc, char** argv )
     int my_rank = 0 ;
     int nb_proc = 1 ;
     MPI_Comm_size(MPI_COMM_WORLD,&nb_proc) ;
-    MPI_Comm_size(MPI_COMM_WORLD,&my_rank) ;
+    MPI_Comm_rank(MPI_COMM_WORLD,&my_rank) ;
 
     if(my_rank==0)
     {
@@ -66,69 +67,217 @@ int main( int argc, char** argv )
       cout<<"NB CHANNELS : "<<image.channels()<<std::endl ;
       cout<<"NROWS       : "<<image.rows<<std::endl ;
       cout<<"NCOLS       : "<<image.cols<<std::endl ;
-      const int channels = image.channels();
+      const int nb_channels = image.channels();
 
+      int nb_centroids = vm["kmean-value"].as<int>();
+      std::vector<uchar> centroids;
+      centroids.reserve(nb_centroids*nb_channels);
 
-      if(vm["filter"].as<int>()==1)
+      // flatten image
+      std::vector<uchar> flat_image; //(image.rows*image.cols*nb_channels);
+      if(image.isContinuous())
       {
-        switch(channels)
-        {
-          case 1:
-            for(std::size_t i=1;i<image.rows-1;++i)
-            {
-              for(int j=1;j<image.cols-1;++j)
-              {
-                array<uchar,5> buffer ;
-                buffer[0] = image.at<uchar>(i-1,j) ;
-                buffer[1] = image.at<uchar>(i,j-1) ;
-                buffer[2] = image.at<uchar>(i,j) ;
-                buffer[3] = image.at<uchar>(i,j+1) ;
-                buffer[4] = image.at<uchar>(i+1,j) ;
-                std::sort(buffer.begin(),buffer.end()) ;
-                image.at<uchar>(i,j) = buffer[2] ;
-              }
-            }
-            break ;
-          case 3:
-            Mat_<Vec3b> _I = image;
-            for(std::size_t i=1;i<image.rows-1;++i)
-            {
-              for(int j=1;j<image.cols-1;++j)
-              {
-                for(int k=0;k<3;++k)
-                {
-                  array<uchar,5> buffer ;
-                  buffer[0] = _I(i-1,j)[k] ;
-                  buffer[1] = _I(i,j-1)[k] ;
-                  buffer[2] = _I(i,j)[k] ;
-                  buffer[3] = _I(i,j+1)[k] ;
-                  buffer[4] = _I(i+1,j)[k] ;
-                  std::sort(buffer.begin(),buffer.end()) ;
-                  _I(i,j)[k] = buffer[2] ;
-                }
-              }
-            }
-            break ;
-        }
-
-        imwrite("./Filter_Image.jpg",image) ;
+	flat_image.assign(image.datastart, image.dataend);
       }
 
-      if(vm["parallel-filter"].as<int>()==1)
+
+
+      if(vm["seg"].as<int>()==1)
       {
+	MPI_Status status;
+	{
+	  // BCAST NB_CHANNELS, NB_ROWS, NB_COLS AND NB_CENTROIDS TO ALL PROCS
+	  std::vector<int> send_info={nb_channels, image.rows, image.cols, nb_centroids};
+	  MPI_Bcast(send_info.data(), 4, MPI_INT, 0, MPI_COMM_WORLD);
+	}
+
+	int rows_local_proc_zero;
+
         {
-          // SEND IMAGE DATA TO OTHER PROC
+          // SEND IMAGE DATA
+	  // send local image rows
+
+	  int begin = 0; 
+	  
+	  rows_local_proc_zero = image.rows / nb_proc;
+	  int r = image.rows % nb_proc;
+	  if(r>0) rows_local_proc_zero ++;
+	  begin += rows_local_proc_zero;
+	  
+
+	  for(int i=1; i<nb_proc; i++)
+	  {
+	    // send local size to proc i
+	    int rows_local = image.rows / nb_proc;
+	    int r = image.rows % nb_proc;
+	    if(r>i) rows_local ++;
+	    MPI_Send(&rows_local, 1, MPI_INT, i, 1000, MPI_COMM_WORLD);
+
+	    // send local part of image
+	    uchar* ptr = flat_image.data() + begin*image.cols*nb_channels;
+	    MPI_Send(ptr, rows_local*image.cols*nb_channels, MPI_UNSIGNED_CHAR, i, 2000, MPI_COMM_WORLD);
+	    begin += rows_local;
+	  }
+        }
+
+	{
+	  // INIT CENTROIDS
+	  // determine number of centroids to init by proc 0 
+	  int nb_cent_to_init_zero = nb_centroids / nb_proc;
+	  int r = nb_centroids % nb_proc;
+	  if(r>0) nb_cent_to_init_zero ++;
+
+	  // init centroids of proc 0
+	  for(int i=0; i<nb_cent_to_init_zero; i++)
+	  {
+	    std::vector<uchar> cent(nb_channels);
+	    cent = init_centroid(flat_image, nb_channels);
+	    centroids.insert(centroids.end(), cent.begin(), cent.end());
+	  }
+
+
+	  // determine number of centroids to init by proc i and send it
+	  for(int i=1; i<std::min(nb_centroids,nb_proc); i++)
+	  {
+	    int nb_cent_to_init = nb_centroids / nb_proc;
+	    int r = nb_centroids % nb_proc;
+	    if(r>i) nb_cent_to_init ++;
+
+	    MPI_Send(&nb_cent_to_init, 1, MPI_INT, i, 3000, MPI_COMM_WORLD);
+
+	    // receive initialized centroids from procs
+	    std::vector<uchar> centroids_local_to_receive(nb_channels*nb_cent_to_init);
+	    MPI_Recv(centroids_local_to_receive.data(), nb_channels*nb_cent_to_init, MPI_UNSIGNED_CHAR, i, 4000, MPI_COMM_WORLD, &status);
+	
+	    // insert received centroids to the vector centroid
+	    centroids.insert(centroids.end(), centroids_local_to_receive.begin(), centroids_local_to_receive.end());
+	  }
+
+	}
+      
+
+        {
+          // COMPUTE CENTROIDS
+          std::vector<int> pixel_collection(image.rows*image.cols);
+	  int iter = 0;
+	  double epsilon = 1;
+	  while( (iter<50) && (epsilon>0.05) )
+	  {
+            std::cout << "iteration number: " << iter << std::endl;
+
+
+	    // broad cast centroids vectors to all procs
+	    MPI_Bcast(centroids.data(), nb_centroids*nb_channels, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+	    // calculate nearest centroid for each pixel
+	    // each proc calculates for the pixel in his part of image
+	    // nearest centroids of the part of proc 0
+	    int nearest_centroid_id;
+	    for(int i=0; i<rows_local_proc_zero*image.cols; i++)
+	    {
+	      switch(nb_channels)
+	      {
+		case(1):
+		{
+		  nearest_centroid_id = nearest_centroid(nb_channels, nb_centroids, centroids, flat_image[i]);
+		  pixel_collection.at(i) = nearest_centroid_id;
+		  break;
+		}
+		case(3):
+		{
+		  nearest_centroid_id = nearest_centroid(nb_channels, nb_centroids, centroids, flat_image[3*i], flat_image[3*i+1], flat_image[3*i+2]);
+		  pixel_collection.at(i) = nearest_centroid_id;
+		  break; 
+		}
+	      }
+	    }
+
+	    // receive calculation of nearest centroids by other procs
+	    int begin = image.cols*rows_local_proc_zero;
+	    for(int i=1; i<nb_proc; i++)
+	    {
+		int rows_local = image.rows / nb_proc;
+	        int r = image.rows % nb_proc;
+	        if(r>i) rows_local ++;
+
+		std::vector<int> pixel_collection_local(image.cols*rows_local);
+
+		MPI_Recv(pixel_collection_local.data(), image.cols*rows_local, MPI_INT, i, 5000, MPI_COMM_WORLD, &status);
+
+		// add the received local pixel collection to the main pixel collection
+		std::copy(pixel_collection_local.begin(), pixel_collection_local.end(), pixel_collection.begin()+begin);
+		begin += image.cols*rows_local;
+
+	    }
+
+
+	    // UPDATING CENTROIDS
+	    std::vector<double> count_pxl;
+	    count_pxl.reserve((nb_channels+1)*nb_centroids);
+	    count_pxl = count_pixels(nb_centroids, pixel_collection, flat_image, rows_local_proc_zero, image.cols, nb_channels);
+
+	    // receive count_pxl_local from each proc
+	    for(int i=1; i<nb_proc; i++)
+	    {
+		std::vector<double> count_pxl_local((nb_channels+1)*nb_centroids);
+		MPI_Recv(count_pxl_local.data(), (nb_channels+1)*nb_centroids, MPI_DOUBLE, i, 6000, MPI_COMM_WORLD, &status);
+
+		// add received count_pxl_local to count_pxl global
+		std::transform(count_pxl.begin(), count_pxl.end(), count_pxl_local.begin(), count_pxl.begin(), std::plus<double>());
+
+
+	    }
+
+
+	    // save old centroids before updating them to calculate variance
+	    std::vector<uchar> old_centroids = centroids;
+
+
+	    // compute barycentre and update centroids
+	    centroids = compute_barycenter_and_update_centroids(count_pxl, nb_centroids, nb_channels);
+	
+	    epsilon = compute_variance(old_centroids, centroids, nb_channels, nb_centroids);
+            std::cout << "epsilon: " << epsilon << std::endl;
+
+	    // send epsilon to other procs
+	    MPI_Bcast(&epsilon, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	    
+	    iter ++;
+	    std::cout << "--------------------------------" << std::endl;
+	  }
         }
 
         {
-          // PARALLEL FILTER
+          // COMPUTE SEGMENTATION
+	  // Bcast final centroids
+	  MPI_Bcast(centroids.data(), nb_centroids*nb_channels, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+	
+	  // compute segmentation by proc 0
+	  compute_segmentation(flat_image, rows_local_proc_zero, image.cols, nb_channels, nb_centroids, centroids);
+
+	  // receive segmentation results by other procs
+	  int begin = image.cols*rows_local_proc_zero*nb_channels;
+	  for(int i=1; i<nb_proc; i++)
+	  {
+	    // receive flat_image_local after segmentation
+	    int rows_local = image.rows / nb_proc;
+	    int r = image.rows % nb_proc;
+	    if(r>i) rows_local ++;
+
+	    std::vector<uchar> flat_image_local(rows_local*image.cols*nb_channels);
+	    MPI_Recv(flat_image_local.data(), rows_local*image.cols*nb_channels, MPI_UNSIGNED_CHAR, i, 7000, MPI_COMM_WORLD, &status);
+
+	    // add flat_image_local to flat_image
+	    std::copy(flat_image_local.begin(), flat_image_local.end(), flat_image.begin()+begin);
+	    begin += image.cols*rows_local*nb_channels;
+	  }
+
         }
 
-        {
-          //COLLECT RESULT DATA
-        }
+	// unflat image
+	unflat_image(flat_image, image, nb_channels);
 
-        imwrite("./Parallel_Filter_Image.jpg",image) ;
+        imwrite("../MPI_Seg_Image.jpg",image) ;
 
       }
 
@@ -137,21 +286,116 @@ int main( int argc, char** argv )
     {
       // MY RANK different 0
 
-      if(vm["parallel-filter"].as<int>()==1)
+      MPI_Status status;
+
+      if(vm["seg"].as<int>()==1)
       {
+	// RECEIVE NB_CHANNELS, NB_ROWS, NB_COLS AND NB_CENTROIDS FROM PROC 0
+	std::vector<int> recv_info(4);
+	MPI_Bcast(recv_info.data(), 4, MPI_INT, 0, MPI_COMM_WORLD);
+	int nb_channels = recv_info[0];
+	int nb_rows = recv_info[1];
+	int nb_cols = recv_info[2];
+	int nb_centroids = recv_info[3];
+
+        
+        // RECV IMAGE DATA FROM PROC 0
+	int rows_local;
+	MPI_Recv(&rows_local, 1, MPI_INT, 0, 1000, MPI_COMM_WORLD, &status);
+	
+	std::vector<uchar> flat_image_local(rows_local*nb_cols*nb_channels);
+	MPI_Recv(flat_image_local.data(), rows_local*nb_cols*nb_channels, MPI_UNSIGNED_CHAR, 0, 2000, MPI_COMM_WORLD, &status);
+
+	{
+       	  // INIT CENTROIDS
+	  // receive nb_centroids to init
+	  if(my_rank<std::min(nb_proc,nb_centroids))
+	  {
+	    int nb_centroids_to_init;
+	    MPI_Recv(&nb_centroids_to_init, 1, MPI_INT, 0, 3000, MPI_COMM_WORLD, &status);
+	    std::vector<uchar> centroids_local;
+	    centroids_local.reserve(nb_centroids_to_init*nb_channels);
+
+	    for(int i=0; i<nb_centroids_to_init; i++)
+	    {
+	      std::vector<uchar> cent(nb_channels);
+	      cent = init_centroid(flat_image_local, nb_channels);
+	      centroids_local.insert(centroids_local.end(), cent.begin(), cent.end());
+
+	    }
+	    
+	    // send initialized centroids to proc 0 
+	    MPI_Send(centroids_local.data(), nb_channels, MPI_UNSIGNED_CHAR, 0, 4000, MPI_COMM_WORLD);
+
+	  }
+	}
+
         {
-          // RECV IMAGE DATA FROM PROC 0
+	  int iter = 0;
+	  //double epsilon = 1;
+	  while (iter<50) 
+	  {
+            // receive centroids vector
+	    std::vector<uchar> centroids(nb_centroids*nb_channels);
+	    MPI_Bcast(centroids.data(), nb_centroids*nb_channels, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+
+	    // calculate nearest centroid
+	    int nearest_centroid_id;
+	    std::vector<int> pixel_collection_local(nb_cols*rows_local);
+	    for(int i=0; i<nb_cols*rows_local; i++)
+	    {
+	      switch(nb_channels)
+	      {
+		case(1):
+		{
+		  nearest_centroid_id = nearest_centroid(nb_channels, nb_centroids, centroids, flat_image_local[i]);
+		  pixel_collection_local.at(i) = nearest_centroid_id;
+		  break;
+		}
+		case(3):
+		{
+		  nearest_centroid_id = nearest_centroid(nb_channels, nb_centroids, centroids, flat_image_local[3*i], flat_image_local[3*i+1], flat_image_local[3*i+2]);
+		  pixel_collection_local.at(i) = nearest_centroid_id;
+		  break; 
+		}
+	      }
+
+	    }
+
+	    // send pixel_collection_local to proc 0
+	    MPI_Send(pixel_collection_local.data(), nb_cols*rows_local, MPI_INT, 0, 5000, MPI_COMM_WORLD);
+
+
+	    // UPDATING CENTROIDS
+	    // count pixels
+	    std::vector<double> count_pxl_local;
+	    count_pxl_local.reserve((nb_channels+1)*nb_centroids);
+	    count_pxl_local = count_pixels(nb_centroids, pixel_collection_local, flat_image_local, rows_local, nb_cols, nb_channels);
+
+	    // send count_pxl_local to proc 0
+	    MPI_Send(count_pxl_local.data(), (nb_channels+1)*nb_centroids, MPI_DOUBLE, 0, 6000, MPI_COMM_WORLD);
+
+	    // receive epsilon value
+	    double epsilon;
+	    MPI_Bcast(&epsilon, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+	    if(epsilon<=0.05) break;
+	    iter++;
+	  }
         }
 
         {
-          // PARALLEL FILTER
-        }
-
-        {
-          //SEND RESULT DATA TO PROC 0
+          // COMPUTE SEGMENTATION
+	  // Bcast final centroid vect from proc 0
+	  std::vector<uchar> centroids(nb_centroids*nb_channels);
+	  MPI_Bcast(centroids.data(), nb_centroids*nb_channels, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+	  // compute segmentation by each proc
+	  compute_segmentation(flat_image_local, rows_local, nb_cols, nb_channels, nb_centroids, centroids);
+	  // send segmentation results to proc 0
+	  MPI_Send(flat_image_local.data(), rows_local*nb_cols*nb_channels, MPI_UNSIGNED_CHAR, 0, 7000, MPI_COMM_WORLD);
         }
       }
     }
+    MPI_Finalize();
     return 0 ;
 }
 
