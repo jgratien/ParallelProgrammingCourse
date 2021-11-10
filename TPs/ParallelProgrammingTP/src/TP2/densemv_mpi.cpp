dcc53984a34a4fc58ac053ce80cbbc9b6c3bd870
Jean-Marc GRATIEN <jean-marc.gratien@ifpen.fr>
2021-11-10 13:42:43 +0100

    update

--- TPs/ParallelProgrammingTP/src/TP2/densemv_mpi.cpp
+++ TPs/ParallelProgrammingTP/src/TP2/densemv_mpi.cpp
@@ -51,7 +51,7 @@ int main(int argc, char** argv)
   int my_rank = 0 ;
   int nb_proc = 1 ;
   MPI_Comm_size(MPI_COMM_WORLD,&nb_proc) ;
-  MPI_Comm_size(MPI_COMM_WORLD,&my_rank) ;
+  MPI_Comm_rank(MPI_COMM_WORLD,&my_rank) ;
 
   using namespace PPTP ;
 
