d9c779b5738dfb0f7b31fc3adf2f304a1ae65db1
Abdeljelil MAADI <abdeljelil.maadi@front-in1.cemef>
2021-12-14 16:56:28 +0100

    parallelisation OMP on life of boids

--- Projects/LiveOfBoids/src/main_profiling.cpp
+++ Projects/LiveOfBoids/src/main_profiling.cpp
@@ -1,6 +1,3 @@
-#ifndef __GNUC__
-#pragma region declarations
-#endif
 #include <glad/glad.h>
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
@@ -9,7 +6,9 @@
 #include <iomanip>
 #include <iostream>
 #include <sstream>
+#include <omp.h>
 #include <vector>
+
 #include "lib/myMath/Vec2.hpp"
 #include "lib/myMath/utils.hpp"
 #include "resources/model/Flock.hpp"
@@ -18,17 +17,20 @@
 #include "resources/graphics/GraphicalManager.hpp"
 #include "resources/graphics/oglTypes.hpp"
 #include "resources/controller/flock_generator.hpp"
-#ifndef __GNUC__
-#pragma endregion
-#endif
+#include "lib/Timers/timers.hpp"
+
+#define NB_THREADS 4
+
 
 Flock* MAIN_pFLOCK = nullptr;
 std::vector<Agent*> mainFlock;
 
 int main() {
 
-    int size = 600;
+    int size = 1000;
     mainFlock.reserve(size);
+
+    PPTP::Timer timer;
 
 
 
@@ -36,24 +38,49 @@ int main() {
 
 
     MAIN_pFLOCK = &flock;
+    {
+        PPTP::Timer::Sentry sentry(timer, "Sequentiel");
+        long int t = 0;
+        do {
 
-    long int t = 0;
-    do {
-        std::cout << "Tour " << t << '\n';
-        for (auto& bird : *MAIN_pFLOCK) {
-            std::tuple<std::vector<Agent*>, std::vector<Agent*>> allNeighbors =
-                (*MAIN_pFLOCK).computeNeighbors(*bird); //this costs performance
-            std::vector<Agent*> bVec = std::get<0>(allNeighbors);
-            std::vector<Agent*> eVec = std::get<1>(allNeighbors);
+            for (auto& bird : *MAIN_pFLOCK) {
+                std::tuple<std::vector<Agent*>, std::vector<Agent*>> allNeighbors =
+                    (*MAIN_pFLOCK).computeNeighbors(*bird); //this costs performance
+                std::vector<Agent*> bVec = std::get<0>(allNeighbors);
+                std::vector<Agent*> eVec = std::get<1>(allNeighbors);
 
-            (*bird).computeLaws(bVec, eVec);
-            (*bird).prepareMove();
-            (*bird).setNextPosition(keepPositionInScreen((*bird).getNextPosition(), 800, 800));
-            (*bird).move();
-        }
-        ++t;
-    } while (t <= 100);
+                (*bird).computeLaws(bVec, eVec);
+                (*bird).prepareMove();
+                (*bird).setNextPosition(keepPositionInScreen((*bird).getNextPosition(), 800, 800));
+                (*bird).move();
+            }
+            ++t;
+        } while (t <= 100);
+    }
+
+    {
+        PPTP::Timer::Sentry sentry(timer, "OMP");
+        long int t = 0;
+        do {
+            std::cout << "Tour " << t << '\n';
+
+            #pragma omp parallel for shared(MAIN_pFLOCK) num_threads(4)
+            for (auto& bird : *MAIN_pFLOCK) {
+                std::tuple<std::vector<Agent*>, std::vector<Agent*>> allNeighbors =
+                    (*MAIN_pFLOCK).computeNeighbors(*bird); //this costs performance
+                std::vector<Agent*> bVec = std::get<0>(allNeighbors);
+                std::vector<Agent*> eVec = std::get<1>(allNeighbors);
+
+                (*bird).computeLaws(bVec, eVec);
+                (*bird).prepareMove();
+                (*bird).setNextPosition(keepPositionInScreen((*bird).getNextPosition(), 800, 800));
+                (*bird).move();
+            }
+            ++t;
+        } while (t <= 100);
+    }
+
+    timer.printInfo();
 
     return 0;
 }
-
