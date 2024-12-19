359ef1f625b174ce6b11dd9c02bc9630454a7686
Ossara DJAODOH <ossara.djaodoh@mines-paristech.fr>
2024-12-19 16:38:32 +0100

    update densemv_mpi

--- TPs/ParallelProgrammingTP/src/TP2/densemv_mpi.cpp
+++ TPs/ParallelProgrammingTP/src/TP2/densemv_mpi.cpp
@@ -109,9 +109,20 @@ int main(int argc, char** argv)
     for(std::size_t i=0;i<nrows;++i)
       x[i] = i+1 ;
 
+          
+    size_t local_size = nrows/nb_proc ;
+    int rest = nrows%nb_proc ;
+
     {
 
       // SEND GLOBAL SIZE
+      MPI_Bcast(&nrows, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD) ;
+      size_t offset = 0 ;
+      {
+        size_t local_nrows = local_size ;
+        if(0 < rest) local_nrows ++ ;
+        offset += local_size*nrows ;
+      }
 
 
       // SEND MATRIX
@@ -120,14 +131,21 @@ int main(int argc, char** argv)
         std::cout<<" SEND MATRIX DATA to proc "<<i<<std::endl ;
 
         // SEND LOCAL SIZE to PROC I
+        size_t local_nrows = local_size ;
+        if(i < rest) local_nrows ++ ;
+        MPI_Send(&local_nrows, 1, MPI_UNSIGNED_LONG, i, 100, MPI_COMM_WORLD) ;
+        std::cout << "send local size = " << local_nrows << " to " << i << std::endl ;
 
         // SEND MATRIX DATA
+        MPI_Send(matrix.data()+offset, local_nrows*nrows, MPI_DOUBLE, i, 101, MPI_COMM_WORLD) ;
+        offset += local_nrows*nrows ;
       }
     }
 
     {
       // BROAD CAST VECTOR X
       /* ... */
+      MPI_Bcast(x.data(), nrows, MPI_DOUBLE, 0, MPI_COMM_WORLD) ; 
     }
 
     {
@@ -144,11 +162,14 @@ int main(int argc, char** argv)
     // COMPUTE LOCAL MATRICE LOCAL VECTOR ON PROC 0
     DenseMatrix local_matrix ;
     std::size_t local_nrows ;
+    std::vector<double> fuse_y(nrows) ;
 
     {
       // EXTRACT LOCAL DATA FROM MASTER PROC
 
       // COMPUTE LOCAL SIZE
+      local_nrows = local_size ;
+      if(0 < rest) local_nrows ++ ;
 
       // EXTRACT LOCAL MATRIX DATA
     }
@@ -156,7 +177,35 @@ int main(int argc, char** argv)
     std::vector<double> local_y(local_nrows);
     {
       // compute parallel SPMV
+      double const* matrix_ptr = matrix.data() ;
+      for(std::size_t irow =0; irow<local_nrows;++irow)
+      {
+        double value = 0 ;
+        for(std::size_t jcol =0; jcol<nrows;++jcol)
+        {
+        value += matrix_ptr[jcol]*x[jcol] ;
+        }
+        local_y[irow] = value ;
+        matrix_ptr += nrows ;
+      }
     }
+    fuse_y = local_y ;
+
+    {
+        MPI_Status status ;
+        for(int i=1; i<nb_proc; ++i)
+        {
+            size_t local_nrows = local_size ;
+            if(i < rest) local_nrows ++ ;
+            std::vector<double> local_y(local_nrows);
+
+            MPI_Recv(local_y.data(), local_nrows, MPI_DOUBLE, i, 102 ,MPI_COMM_WORLD, &status) ;
+            fuse_y.insert(fuse_y.end(), local_y.begin(), local_y.end());
+        }
+    }
+
+    double normy = PPTP::norm2(fuse_y) ;
+    std::cout<<"||y||="<<normy<<std::endl ;
   }
   else
   {
@@ -165,29 +214,52 @@ int main(int argc, char** argv)
     DenseMatrix local_matrix ;
     std::size_t nrows ;
     std::size_t local_nrows ;
+    MPI_Status status;
 
     {
       // RECV DATA FROM MASTER PROC
 
       // RECV GLOBAL SIZE
+      MPI_Bcast(&nrows, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD) ;
+      std::cout << "global size value receive by " << my_rank << " is " << nrows << std::endl ;
 
       // RECV LOCAL SIZE
+      MPI_Recv(&local_nrows, 1, MPI_UNSIGNED_LONG, 0, 100, MPI_COMM_WORLD, &status) ;
+      std::cout << "local size value receive by " << my_rank << " is " << local_nrows << std::endl ;
 
       // RECV MATRIX DATA
+      local_matrix.init(local_nrows*nrows) ;
+      MPI_Recv(local_matrix.data(), local_nrows*nrows, MPI_DOUBLE, 0, 101, MPI_COMM_WORLD, &status) ;
     }
 
     std::vector<double> x;
     {
       // BROAD CAST VECTOR X
       /* ... */
+      x.resize(nrows) ;
+      MPI_Bcast(x.data(), nrows, MPI_DOUBLE, 0, MPI_COMM_WORLD) ; 
     }
 
-    std::vector<double> local_y(local_nrows);
+    std::vector<double> local_y(local_nrows) ;
     {
       // compute parallel SPMV
+      double const* matrix_ptr = local_matrix.data() ;
+      for(std::size_t irow =0; irow<local_nrows;++irow)
+      {
+        double value = 0 ;
+        for(std::size_t jcol =0; jcol<nrows;++jcol)
+        {
+        value += matrix_ptr[jcol]*x[jcol] ;
+        }
+        local_y[irow] = value ;
+        matrix_ptr += nrows ;
+      }
     }
+
+    MPI_Send(local_y.data(), local_nrows, MPI_DOUBLE, 0, 102, MPI_COMM_WORLD) ;
 
   }
   timer.printInfo() ;
+  MPI_Finalize() ;
   return 0 ;
 }
